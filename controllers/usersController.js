806e9c8e6ec08822e99c55194d55c7bd95b54afb
Saran barua <Saranbarua2357@gmail.com>
2022-08-21 14:45:59 +0600

    users updated

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,7 +1,35 @@
+//external import
+const bcrypt = require("bcrypt")
+
+
 function getUsers(req,res,next){
     res.render("users")
     }
     
+// add users function
+async function addusers(req,res, next){
+   let newUser;
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+   
+  if(req.files && req.files.length >0){
+    newUser = new User ({
+      ...req.body,  //fetch req body
+      avatar : req.files[0].filename,
+      password: hashedPassword,
+    })
+ }
+    else{
+        newUser = new User ({
+            ...req.body,  //fetch req body
+            password: hashedPassword,
+         })
+
+    }
+    
+  }
+
+
+
     module.exports ={
-        getUsers
+        getUsers, addusers,
     }
