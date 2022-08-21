6114842c960421fcbc4358cfd041249213a948db
Saran barua <Saranbarua2357@gmail.com>
2022-08-21 14:15:17 +0600

    controller add

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,11 +1,17 @@
 //external import
 const express= require('express') 
 const router= express.Router();
-const decorateHtmlResponse= require('../middlewares/common/decorateHtmlResponse')
 
 //internal import
 const {getUsers} = require("../controllers/usersController")
+const decorateHtmlResponse= require('../middlewares/common/decorateHtmlResponse')
+const avatarUpload = require ("../middlewares/users/avatarUpload")
+const {addUserValidators, addUserValidationHandler}  = require("../middlewares/users/usersValidator")
+
 //users in page
 router.get("/" ,decorateHtmlResponse ("User"), getUsers)
+
+//add user""
+router.post("/", avatarUpload,addUserValidators,addUserValidationHandler )
 
 module.exports= router;
