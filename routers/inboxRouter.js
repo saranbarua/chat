4609494bd0675032a67c0a530c104196fa7cf0d6
Saran barua <Saranbarua2357@gmail.com>
2022-08-24 12:20:15 +0600

    nwe

--- routers/inboxRouter.js
+++ routers/inboxRouter.js
@@ -4,9 +4,29 @@ const router= express.Router();
 const decorateHtmlResponse= require('../middlewares/common/decorateHtmlResponse')
 
 //internal import
-const {getInbox} = require("../controllers/inboxController")
+const {getInbox,
+       searchUser,
+       addConversation,
+    getMessages,
+   sendMessage  
+}= require("../controllers/inboxController")
 const {checkLogin}= require("../middlewares/common/checkLogIn")
+const attachmentUpload = require("../middlewares/inbox/attachmentUpload");
 //log in page
 router.get("/" , decorateHtmlResponse ("Inbox"),checkLogin, getInbox)
+
+
+// search user for conversation
+router.post("/search", checkLogin, searchUser);
+
+// add conversation
+router.post("/conversation", checkLogin, addConversation);
+
+// get messages of a conversation
+router.get("/messages/:conversation_id", checkLogin, getMessages);
+
+// send message
+router.post("/message", checkLogin, attachmentUpload, sendMessage);
+
 
 module.exports= router;
